456e564a4ba8be1b7c70a4212301ee11045d845f
Sven Rademakers <sven.rademakers@gmail.com>
2018-04-17 21:11:58 +0200

    AddMicroTetst.cmake: point to googletest submodule

--- cmake_helpers/AddMicroTest.cmake
+++ cmake_helpers/AddMicroTest.cmake
@@ -8,15 +8,16 @@ macro(AddMicroTest target)
     if (${num_extra_args} EQUAL 0)
         message (FATAL_ERROR "no source files passed to AddMicroTest macro")
 	endif ()
-	
+
+	if(WIN32)
+		SET(gtest_force_shared_crt on CACHE BOOL "Use shared (DLL) run-time lib even when Google Test is built as static lib.") 
+	endif()
+	add_subdirectory(${CURR_DIR}/../test/googletest)
+
 	add_executable(${target}_MicroTests ${CURR_DIR}/test.cpp ${extra_macro_args})
 	target_include_directories(${target}_MicroTests
 		PRIVATE
 			$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>)
-			
-	if(WIN32)
-		SET(gtest_force_shared_crt on CACHE BOOL "Use shared (DLL) run-time lib even when Google Test is built as static lib.") 
-	endif()
 	
 	target_link_libraries(${target}_MicroTests ${target} gtest gmock)
 	install(TARGETS ${target}_MicroTests DESTINATION bin)	
