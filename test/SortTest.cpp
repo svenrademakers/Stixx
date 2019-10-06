d21098c30e394d1850afb0119a8f0a11bad70833
Sven Rademakers <sven.rademakers@gmail.com>
2019-10-06 16:06:34 +0200

    sortTest WIP

--- test/SortTest.cpp
+++ test/SortTest.cpp
@@ -1,39 +1,59 @@
 #include <gtest/gtest.h>
 #include "Sort.h"
 #include "Buffer.hpp"
+#include <functional>
 
-TEST(bubbleSortTest, two_value_sort)
+static sx::Buffer<void(sx::MemoryView<int>), 1> sortFunctions()
 {
-	sx::Buffer<int, 2> x = { 3,2 };
-
-	BubbleSort(x);
-	EXPECT_EQ(x[0], 2);
-	EXPECT_EQ(x[1], 3);
+	sx::Buffer<void(sx::MemoryView<int>), 1> wat;
+	wat[0] = BubbleSort;
+	return wat;
 }
+//		= { std::function<void(sx::MemoryView<int>)>(SelectionSort) };
+//= {
+//	BubbleSort
+//};
 
-TEST(bubbleSortTest, two_value_sort_decending)
-{
-	sx::Buffer<int, 2 > x = { 2,3 };
-
-	BubbleSort(sx::MemoryView<int>(x), [](const char& first, const char& second) { return first < second; });
-	EXPECT_EQ(x[0], 3);
-	EXPECT_EQ(x[1], 2);
-}
-
-TEST(bubbleSortTest, one_value)
-{
-	sx::Buffer<int, 1 > x = { 2 };
-
-	BubbleSort(x);
-	EXPECT_EQ(x[0], 2);
-}
-
-TEST(bubbleSortTest, sort_multiple_values)
-{
-	sx::Buffer<int, 4 > x = { 2,3,888888, -44 };
-	BubbleSort(x);
-	EXPECT_EQ(x[0], -44);
-	EXPECT_EQ(x[1], 2);
-	EXPECT_EQ(x[2], 3);
-	EXPECT_EQ(x[3], 888888);
-}
+//class SortingTests :
+//	public testing::TestWithParam<int> {
+//	// You can implement all the usual fixture class members here.
+//	// To access the test parameter, call GetParam() from class
+//	// TestWithParam<T>.
+//};
+//
+//TEST_P(SortingTests, two_value_sort)
+//{
+//	sx::Buffer<int, 2> x = { 3,2 };
+//
+//	auto sortFunction = GetParam();
+//	sortFunction(x);
+//
+//	EXPECT_EQ(x[0], 2);
+//	EXPECT_EQ(x[1], 3);
+//}
+//
+//TEST_P(SortingTests, sort_one_value)
+//{
+//	auto sortFunction = GetParam();
+//
+//	sx::Buffer<int, 1 > x = { 2 };
+//
+//	BubbleSort(x);
+//	EXPECT_EQ(x[0], 2);
+//}
+//
+//TEST_P(SortingTests, sort_with_negative_numbers)
+//{
+//	auto sortFunction = GetParam();
+//
+//	sx::Buffer<int, 4 > x = { 2,3,888888, -44 };
+//	BubbleSort(x);
+//	EXPECT_EQ(x[0], -44);
+//	EXPECT_EQ(x[1], 2);
+//	EXPECT_EQ(x[2], 3);
+//	EXPECT_EQ(x[3], 888888);
+//}
+//
+//INSTANTIATE_TEST_CASE_P(BubbleSortTest, 
+//	SortingTests, 
+//	::testing::Values(bubbleSortF));
